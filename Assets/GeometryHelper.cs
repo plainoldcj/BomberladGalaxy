3e64c6e52f2749ca1a53066921220eb3e4259a5f
Christian Jaeger <johndoe.foobar@googlemail.com>
2015-07-16 15:44:02 +0200

    refactored CreateMesh to CreatePlane

---
+++ Assets/GeometryHelper.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeometryHelper : MonoBehaviour {
+
+	public static Mesh CreatePlane(int numEdgeVerts, float edgeLength) {
+		int numVerts = numEdgeVerts * numEdgeVerts;
+		
+		float ds = edgeLength / (numEdgeVerts - 1);
+		float dt = 1.0f / (numEdgeVerts - 1);
+		
+		// create vertices
+		
+		// row-major grid layout
+		Vector3[] vertices = new Vector3[numVerts];
+		Vector2[] texCoords = new Vector2[numVerts];
+		
+		Vector3 pos = Vector3.zero;
+		Vector2 tcoord = Vector2.zero;
+		for(int i = 0; i < numEdgeVerts; ++i) {
+			for(int j = 0; j < numEdgeVerts; ++j) {
+				int vidx = numEdgeVerts * i + j;
+				vertices[vidx] = pos;
+				texCoords[vidx] = tcoord;
+				pos.x += ds;
+				tcoord.x += dt;
+			}
+			pos.x = 0.0f;
+			pos.y -= ds;
+			tcoord.x = 0.0f;
+			tcoord.y += dt;
+		}
+		
+		// create indices
+		
+		int numTris = 2 * (numEdgeVerts - 1) * (numEdgeVerts - 1);
+		int numIndices = 3 * numTris;
+		
+		int[] indices = new int[numIndices];
+		
+		int cur = 0;
+		for(int i = 0; i < numEdgeVerts - 1; ++i) {
+			for(int j = 0; j < numEdgeVerts - 1; ++j) {
+				indices[cur + 0] = numEdgeVerts * i + j;
+				indices[cur + 1] = numEdgeVerts * i + (j + 1);
+				indices[cur + 2] = numEdgeVerts * (i + 1) + j;
+				
+				indices[cur + 3] = numEdgeVerts * (i + 1) + j;
+				indices[cur + 4] = numEdgeVerts * i + (j + 1);
+				indices[cur + 5] = numEdgeVerts * (i + 1) + (j + 1);
+				
+				cur += 6;
+			}
+		}
+		
+		Mesh mesh = new Mesh();
+		mesh.vertices = vertices;
+		mesh.uv = texCoords;
+		mesh.triangles = indices;
+		
+		return mesh;
+	}
+
+}
--- Assets/Ground.cs
+++ Assets/Ground.cs
@@ -19,70 +19,16 @@ public class Ground : MonoBehaviour {
 	private Mesh 			m_mesh;
 	private GameObject[]	m_tiles;
 
-	private static Mesh CreateMesh() {
-		const int numEdgeVerts = 50;
-		const int numVerts = numEdgeVerts * numEdgeVerts;
+	private void CreateMesh() {
+		GameObject gameController = GameObject.FindWithTag("GameController");
 		
-		Globals globals = GameObject.FindWithTag("GameController").GetComponent<Globals>();
+		const int numEdgeVerts = 50;
+		
+		Globals globals = gameController.GetComponent<Globals>();
 		float edgeLength = globals.m_tileEdgeLength * globals.m_numTilesPerEdge;
 		
-		float ds = edgeLength / (numEdgeVerts - 1);
-		float dt = 1.0f / (numEdgeVerts - 1);
-		
-		// create vertices
-		
-		// row-major grid layout
-		Vector3[] vertices = new Vector3[numVerts];
-		Vector2[] texCoords = new Vector2[numVerts];
-		
-		Vector3 pos = Vector3.zero;
-		Vector2 tcoord = Vector2.zero;
-		for(int i = 0; i < numEdgeVerts; ++i) {
-			for(int j = 0; j < numEdgeVerts; ++j) {
-				int vidx = numEdgeVerts * i + j;
-				vertices[vidx] = pos;
-				texCoords[vidx] = tcoord;
-				pos.x += ds;
-				tcoord.x += dt;
-			}
-			pos.x = 0.0f;
-			pos.y -= ds;
-			tcoord.x = 0.0f;
-			tcoord.y += dt;
-		}
-		
-		// create indices
-		
-		const int numTris = 2 * (numEdgeVerts - 1) * (numEdgeVerts - 1);
-		const int numIndices = 3 * numTris;
-		
-		int[] indices = new int[numIndices];
-		
-		int cur = 0;
-		for(int i = 0; i < numEdgeVerts - 1; ++i) {
-			for(int j = 0; j < numEdgeVerts - 1; ++j) {
-				indices[cur + 0] = numEdgeVerts * i + j;
-				indices[cur + 1] = numEdgeVerts * i + (j + 1);
-				indices[cur + 2] = numEdgeVerts * (i + 1) + j;
-				
-				indices[cur + 3] = numEdgeVerts * (i + 1) + j;
-				indices[cur + 4] = numEdgeVerts * i + (j + 1);
-				indices[cur + 5] = numEdgeVerts * (i + 1) + (j + 1);
-				
-				cur += 6;
-			}
-		}
-		
-		Mesh mesh = new Mesh();
-		mesh.name = "GroundMesh";
-		mesh.vertices = vertices;
-		mesh.uv = texCoords;
-		mesh.triangles = indices;
-		
-		// logging
-		Debug.Log("ground mesh: " + vertices.Length + " vertices, " + indices.Length + " indices");
-		
-		return mesh;
+		m_mesh = GeometryHelper.CreatePlane(numEdgeVerts, edgeLength);
+		m_mesh.name = "GroundMesh";
 	}
 	
 	private void CreateTiles() {
@@ -113,7 +59,7 @@ public class Ground : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		m_mesh = CreateMesh();
+		CreateMesh();
 		CreateTiles();
 	}
 	
